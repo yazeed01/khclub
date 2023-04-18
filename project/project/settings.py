b77085f45670424129224d9308b96e2b931294e8
Y . A . Z Yazid <yaz7894@gmail.com>
2023-04-18 06:47:27 +0300

    up

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -144,6 +144,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 
 STATIC_URL = '/static/'
 
